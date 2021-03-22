45786bdf8aad7153d5c2bc551f5760e66c78653a
soffet713 <30372648+soffet713@users.noreply.github.com>
2021-03-22 14:38:16 -0400

    Now tests page in both English & Japanese
    Tests math functions page in both English and Japanese and returns to projects top page at end before closing the browser.

--- math_functions.js
+++ math_functions.js
@@ -99,13 +99,13 @@ describe('Test Math Functions Page', function() {
 		while (eng_functions){
 			test_func_single(0, Math.floor(10 + (Math.random() * 91) + 1), 'fibonacci1', 'fib_compute', 'fib_reset', 'fib_top_return');
 			test_func_double(1, Math.floor((Math.random() * 9999999) + 1), Math.floor((Math.random() * 9999999) + 1), 'greatcomdiv1', 'greatcomdiv2', 'gcd_compute', 'gcd_reset', 'gcd_top_return');
-    		test_func_single(2, Math.floor((Math.random() * 20) + 1), 'factorialInput', 'fact_compute', 'fact_reset', 'fact_top_return');
-    		test_func_single(3, Math.floor((Math.random() * 10000000000) + 1), 'primeNumberInput', 'chkprime_compute', 'chkprime_reset', 'chkprime_top_return');
-    		test_func_single(4, Math.floor((Math.random() * 999) + 1), 'numbprimesInput', 'primedisp_compute', 'primedisp_reset', 'primedisp_top_return');
+    			test_func_single(2, Math.floor((Math.random() * 20) + 1), 'factorialInput', 'fact_compute', 'fact_reset', 'fact_top_return');
+    			test_func_single(3, Math.floor((Math.random() * 10000000000) + 1), 'primeNumberInput', 'chkprime_compute', 'chkprime_reset', 'chkprime_top_return');
+    			test_func_single(4, Math.floor((Math.random() * 999) + 1), 'numbprimesInput', 'primedisp_compute', 'primedisp_reset', 'primedisp_top_return');
 
-    		test_string = 'Testing automation with Selenium in Angular';
+    			test_string = 'Testing automation with Selenium in Angular';
 			
-    		test_rev_string(5, test_string, 'stringreverse1', 'reverse_letters', 'reverse_words', 'rev_words_letters', 'orig_string', 'string_reset', 'string_top_return');
+    			test_rev_string(5, test_string, 'stringreverse1', 'reverse_letters', 'reverse_words', 'rev_words_letters', 'orig_string', 'string_reset', 'string_top_return');
 			jp_functions = eng_functions;
 			eng_functions = false;
 		};
@@ -119,16 +119,18 @@ describe('Test Math Functions Page', function() {
 		while (jp_functions){
 			test_func_single(6, Math.floor(10 + (Math.random() * 91) + 1),'fibonacciJ1', 'fib_computeJ', 'fib_resetJ', 'fib_top_returnJ');
 			test_func_double(7, Math.floor((Math.random() * 9999999) + 1), Math.floor((Math.random() * 9999999) + 1), 'greatcomdivJ1', 'greatcomdivJ2', 'gcd_computeJ', 'gcd_resetJ', 'gcd_top_returnJ');
-    		test_func_single(8, Math.floor((Math.random() * 20) + 1), 'factorialJInput', 'fact_computeJ', 'fact_resetJ', 'fact_top_returnJ');
-    		test_func_single(9, Math.floor((Math.random() * 10000000000) + 1), 'primeNumberJInput', 'chkprime_computeJ', 'chkprime_resetJ', 'chkprime_top_returnJ');
-    		test_func_single(10, Math.floor((Math.random() * 999) + 1), 'numbprimesJInput', 'primedisp_computeJ', 'primedisp_resetJ', 'primedisp_top_returnJ');
+    			test_func_single(8, Math.floor((Math.random() * 20) + 1), 'factorialJInput', 'fact_computeJ', 'fact_resetJ', 'fact_top_returnJ');
+    			test_func_single(9, Math.floor((Math.random() * 10000000000) + 1), 'primeNumberJInput', 'chkprime_computeJ', 'chkprime_resetJ', 'chkprime_top_returnJ');
+    			test_func_single(10, Math.floor((Math.random() * 999) + 1), 'numbprimesJInput', 'primedisp_computeJ', 'primedisp_resetJ', 'primedisp_top_returnJ');
 
-    		test_stringJ = 'アンギュラージェイエスのプログラミング言語でSeleniumのテストスクリプトを勉強してる。';
+    			test_stringJ = 'アンギュラージェイエスのプログラミング言語でSeleniumのテストスクリプトを勉強してる。';
 			
-    		test_rev_string(11, test_stringJ, 'stringreverse1J', 'reverse_lettersJ', '', '', 'orig_stringJ', 'string_resetJ', 'string_top_returnJ');
+    			test_rev_string(11, test_stringJ, 'stringreverse1J', 'reverse_lettersJ', '', '', 'orig_stringJ', 'string_resetJ', 'string_top_returnJ');
 			jp_functions = false;
 		}	
 		
+		element(by.xpath('//button[contains(text(),"Projects Top")]')).click();
+		browser.sleep(3500);
 		done();
 	});
 });
